db759edac88e9fdfde95a0b45a343cb56b3434af
anaskhan010 <anas@sentrixmedia.com>
2025-08-04 05:07:57 -0500

    new changes

--- assets/Page-BtOSiYO1.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-h1oYvEiU.js";import{S as s}from"./scroll-area-D3FaqDjI.js";const t=({children:o})=>r.jsx(s,{className:"h-full w-full p-6 overflow-auto",children:o});export{t as P};
---
+++ assets/Page-BxE_ONZq.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-Ds_RFYKE.js";import{S as s}from"./scroll-area-BWC8hO50.js";const t=({children:o})=>r.jsx(s,{className:"h-full w-full p-6 overflow-auto",children:o});export{t as P};
