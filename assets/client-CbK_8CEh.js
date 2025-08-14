9bdf87fd78ef0f479b814a2b9a936e22e6ada345
anaskhan010 <anas@sentrixmedia.com>
2025-08-14 12:15:20 -0500

    new changes

---
+++ assets/client-CbK_8CEh.js
@@ -0,0 +1,2 @@
+import{d as a}from"./index-D4E5b_kP.js";const s=a;export{s as a};
+//# sourceMappingURL=client-CbK_8CEh.js.map
--- assets/client-Dtmn2B7S.js
+++
@@ -1,2 +0,0 @@
-import{d as a}from"./index-D5ZEe98-.js";const s=a;export{s as a};
-//# sourceMappingURL=client-Dtmn2B7S.js.map
