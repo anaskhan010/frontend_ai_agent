5c562dcfe144d80c07c6dda4dec7b18cc3c10083
anaskhan010 <anas@sentrixmedia.com>
2025-09-01 08:33:09 -0500

    new changes

---
+++ assets/client-B_zE-fvr.js
@@ -0,0 +1,2 @@
+import{d as a}from"./index-3O6fgkW-.js";const s=a;export{s as a};
+//# sourceMappingURL=client-B_zE-fvr.js.map
--- assets/client-BmkxL5cd.js
+++
@@ -1,2 +0,0 @@
-import{d as a}from"./index-DFZfXj0R.js";const s=a;export{s as a};
-//# sourceMappingURL=client-BmkxL5cd.js.map
