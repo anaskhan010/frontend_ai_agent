6ff6ca240e1a94bf72738fb9f13accf7a4475e5b
anaskhan010 <anas@sentrixmedia.com>
2025-09-25 15:15:59 +0500

    new changes

--- assets/client-2idf8d8P.js
+++
@@ -1,2 +0,0 @@
-import{d as a}from"./index-m2-1zBbl.js";const s=a;export{s as a};
-//# sourceMappingURL=client-2idf8d8P.js.map
---
+++ assets/client-kSK6jMB1.js
@@ -0,0 +1,2 @@
+import{d as a}from"./index-Cm1HGNgs.js";const s=a;export{s as a};
+//# sourceMappingURL=client-kSK6jMB1.js.map
