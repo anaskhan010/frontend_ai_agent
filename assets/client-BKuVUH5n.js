fa2d1ac093e774f8c6042d5b718b03a5cf4d6b7a
anaskhan010 <anas@sentrixmedia.com>
2025-10-17 21:05:51 +0500

    new chnages

---
+++ assets/client-BKuVUH5n.js
@@ -0,0 +1,2 @@
+import{d as a}from"./index-YNP4OU4_.js";const s=a;export{s as a};
+//# sourceMappingURL=client-BKuVUH5n.js.map
--- assets/client-CM-IqsZA.js
+++
@@ -1,2 +0,0 @@
-import{d as a}from"./index-C95eVPAi.js";const s=a;export{s as a};
-//# sourceMappingURL=client-CM-IqsZA.js.map
