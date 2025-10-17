390552092395726283ebdc47c378706920a42203
anaskhan010 <anas@sentrixmedia.com>
2025-10-18 00:33:24 +0500

    new chnages

--- assets/client-DANOwbWQ.js
+++
@@ -1,2 +0,0 @@
-import{d as a}from"./index-Dlzy9K_J.js";const s=a;export{s as a};
-//# sourceMappingURL=client-DANOwbWQ.js.map
---
+++ assets/client-T_stJjnh.js
@@ -0,0 +1,2 @@
+import{d as a}from"./index-CVuMn1_A.js";const s=a;export{s as a};
+//# sourceMappingURL=client-T_stJjnh.js.map
