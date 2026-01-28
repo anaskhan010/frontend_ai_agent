606ec11b967a28bf4f43dff7dfef571de685a147
anaskhan010 <anas@sentrixmedia.com>
2026-01-28 11:44:37 +0500

    new chnages

--- assets/client-Bp7TmO52.js
+++
@@ -1,2 +0,0 @@
-import{a}from"./index-CrtuxIoh.js";const s=a;export{s as a};
-//# sourceMappingURL=client-Bp7TmO52.js.map
---
+++ assets/client-pryM4AcV.js
@@ -0,0 +1,2 @@
+import{a}from"./index-_FoT01MC.js";const s=a;export{s as a};
+//# sourceMappingURL=client-pryM4AcV.js.map
